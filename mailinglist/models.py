14f9f2447412010f5c464bdfb312a8d09111d90f
Innocent Zenda <innnocent@zendainnocent.com>
2020-02-13 18:19:53 +0300

    Create Message model

--- mailinglist/models.py
+++ mailinglist/models.py
@@ -26,3 +26,11 @@ class Subscriber(models.Model):
     class Meta:
         unique_together = ['email', mailing_list]
 
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    mailing_list = models.ForeignKey(MailingList, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=140)
+    body = models.TextField()
+    started = models.DateTimeField(default=None, null=True)
+    finished = models.DateTimeField(default=None, null=True)
