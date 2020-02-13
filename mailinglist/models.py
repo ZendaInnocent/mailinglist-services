1933f4962e568e230ec4d158e281ad8d785fbda8
Innocent Zenda <innnocent@zendainnocent.com>
2020-02-13 18:12:50 +0300

    Create MailingList model

--- mailinglist/models.py
+++ mailinglist/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class MailingList(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=140)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+    def can_user_use_mailing_list(self, user):
+        return user == self.owner
+
