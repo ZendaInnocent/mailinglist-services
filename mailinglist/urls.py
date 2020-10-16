32670accb549ddcbbd531c97ce6196822181616f
Innocent Zenda <zendainnocent@gmail.com>
2020-10-16 09:11:51 +0300

    rendering templates

--- mailinglist/urls.py
+++ mailinglist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from . import views
+
 
 app_name = 'mailinglist'
 
 urlpatterns = [
-    
+    path('', views.MailinglistListView.as_view(), name='mailinglist-list'),
 ]
--- mailinglist/views.py
+++ mailinglist/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from mailinglist.models import MailingList
+
+
+class MailinglistListView(ListView):
+    model = MailingList
